e0c197ce795fad0ff83d1ed30f1aad754a0c0a0f
Elizabeth Sloan <esloan94@gmail.com>
2022-07-11 17:13:43 -0500

    model and article work

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema[7.0].define(version: 2022_07_11_211611) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
 end
