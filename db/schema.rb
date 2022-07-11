47d13405420a226e0c79cfbd60c8da3987260e43
Elizabeth Sloan <esloan94@gmail.com>
2022-07-11 16:22:23 -0500

    Add title and description to table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_11_205657) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_11_211611) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
