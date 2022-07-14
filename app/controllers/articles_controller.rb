56c35eea1e8f0bdd27200adbdef93d281c1c52b2
Elizabeth Sloan <esloan94@gmail.com>
2022-07-14 14:12:02 -0500

    Add links to all pages and 'cancle' buttons

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -40,4 +40,10 @@ class ArticlesController < ApplicationController
 
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path, status: :see_other
+  end
+
 end
